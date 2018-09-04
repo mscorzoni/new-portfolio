0ee27fd0a74c3f0a483f522804e962e7973f9548
Marcos Scorzoni <mlessa@gmail.com>
2018-09-04 09:46:56 +0100

    create portfolio resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_082731) do
+ActiveRecord::Schema.define(version: 2018_09_04_084349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string "title"
