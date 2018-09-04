d3cac8fcab4d9879d3eff9e6489cb2c83122dae3
Marcos Scorzoni <mlessa@gmail.com>
2018-09-04 10:05:40 +0100

    add seed to skills and portfolios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_084349) do
+ActiveRecord::Schema.define(version: 2018_09_04_090436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_09_04_084349) do
     t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "subtitle"
   end
 
   create_table "skills", force: :cascade do |t|
