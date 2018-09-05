1db703506fa5c6fbac02e93fad49d197317bab90
Marcos Scorzoni <mlessa@gmail.com>
2018-09-05 10:17:30 +0100

    add correct email link to footer

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,12 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def modal
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
 end
